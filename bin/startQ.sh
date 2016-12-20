c1d7b1c9d60156d8581cdfa84622a4138eac410c
LaoZhu <shuiyun_5182604@163.com>
2016-12-20 18:32:47 +0800

    add queue count

--- bin/startQ.sh
+++ bin/startQ.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 nohup python ../src/market/queue.py ctp > /tmp/mq.log &
+nohup python ../src/market/queue.py ctp > /tmp/mq.log &
+nohup python ../src/market/queue.py ctp > /tmp/mq.log &
+nohup python ../src/market/queue.py ctp > /tmp/mq.log &
+nohup python ../src/market/queue.py ctp > /tmp/mq.log &
 nohup python ../src/trade/queue.py ctp > /tmp/tq.log &
