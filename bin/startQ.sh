fba1b75d6833d48652687d6c7f732d5a4452b0d0
ctp <ctp@ali.com>
2016-12-22 15:53:35 +0800

    debug

--- bin/startQ.sh
+++ bin/startQ.sh
@@ -6,3 +6,5 @@ nohup python ../src/market/queue.py ctp $1 > /tmp/mq.log &
 nohup python ../src/market/queue.py ctp $1 > /tmp/mq.log &
 nohup python ../src/market/queue.py ctp $1 > /tmp/mq.log &
 nohup python ../src/market/queue.py ctp $1 > /tmp/mq.log &
+nohup python ../src/trade/queue.py ctp $1 > /tmp/tq.log &
+
