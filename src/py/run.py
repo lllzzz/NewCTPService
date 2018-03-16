6e43717c8383462d3602654a2989ee9e61804bd7
laozhu <shuiyun_5182604@163.com>
2018-03-16 23:16:31 +0800

    页面调整

--- src/py/run.py
+++ src/py/run.py
@@ -5,6 +5,7 @@ from web.models.Model import Model
 from web.views.CtpModelView import CtpModelView
 from web.views.TickView import TickView
 from web.views.ConfigView import ConfigView
+from web.views.LBTestView import LBTestView
 from redis import Redis
 from flask_admin.contrib import rediscli
 
@@ -20,6 +21,7 @@ config = Config.get()
 admin.add_view(CtpModelView(Model, db.session))
 admin.add_view(TickView(TickHc, db.session, category='Tick'))
 admin.add_view(ConfigView(name='Config'))
+admin.add_view(LBTestView(name='LBTest'))
 admin.add_view(rediscli.RedisCli(Redis(host=config['redis']['host'], db=config['redis']['db'])))
 
 @app.route('/admin/model/test/', methods=['POST', 'GET'])
--- src/py/web/views/CtpModelView.py
+++ src/py/web/views/CtpModelView.py
@@ -22,35 +22,6 @@ class CtpModelView(ModelView):
     edit_template = 'admin/model_edit.html'
 
 
-    @expose('/lbtest/', methods=('GET', 'POST'))
-    def lbtest(self):
-
-        files = []
-        for file in os.listdir('src/py/web/static/logs/'):
-            if file == '.gitignore': continue
-            files.append(file)
-
-        return self.render('admin/lbtest.html', files=files)
-
-
-    @expose('/lbtest/run/', methods=('GET', 'POST'))
-    def lbtest_run(self):
-
-        app = request.form.get('app')
-        config = Config.get()
-
-        if not config['app'].has_key(app):
-            flash('Please add config', 'error')
-            return redirect('/admin/model/lbtest/')
-
-        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        cmd = 'CTP_CONFIG_PATH=%s python src/py/lbtest.py %s' % (os.environ.get('CTP_CONFIG_PATH'), app)
-        fd = open('src/py/web/static/logs/lbtest_%s_%s.log' % (app, now), 'w')
-        subprocess.Popen(cmd, shell=True, stdout=fd)
-        flash('Test is Runing', 'success')
-        return redirect('/admin/model/lbtest/')
-
-
     @expose('/edit/', methods=('GET', 'POST'))
     def edit_view(self):
 
